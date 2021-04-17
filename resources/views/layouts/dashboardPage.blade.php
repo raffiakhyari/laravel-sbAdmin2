58a6b8b831ac4423aaedf911a79f0e5c6facb1b8
raffiakhyari <muhammadraffi.akhyari@gmail.com>
2021-04-17 21:29:34 +0700

    Co-authored-by: Andreas Asatera <coziboy@users.noreply.github.com>

--- resources/views/layouts/dashboardPage.blade.php
+++ resources/views/layouts/dashboardPage.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,14 +11,13 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
 </head>
 
 <body id="page-top">
@@ -337,7 +335,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Ini masih template</span>
                                 <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                    src="{{ asset('img/undraw_profile.svg') }}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('halaman1', function () {
+Route::get('/dashboard/halaman1', function () {
     return view ('halaman1');
 })-> middleware(['auth'])->name('halaman1');
 
-Route::get('halaman2', function () {
+Route::get('/dashboard/halaman2', function () {
     return view ('halaman2');
 })-> middleware(['auth'])->name('halaman2');
 
-Route::get('halaman3', function () {
+Route::get('/dashboard/halaman3', function () {
     return view ('halaman3');
 })-> middleware(['auth'])->name('halaman3');
 
-Route::get('halaman4', function () {
+Route::get('/dashboard/halaman4', function () {
     return view ('halaman4');
 })-> middleware(['auth'])->name('halaman4');
 
